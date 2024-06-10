31a9eea8a82d834956b89dae24fb83ae506db79a
Moslem Akhoundi <m.akhoundi@ernyka.com>
2024-06-10 16:11:03 +0330

    update-docker

--- back-end/docker-entrypoint.sh
+++ back-end/docker-entrypoint.sh
@@ -4,12 +4,6 @@ set -e
 # Install dependencies with Composer
 composer install
 
-# Run Symfony migrations using the Symfony CLI
-symfony console doctrine:migrations:migrate --no-interaction
-
-# Load Symfony fixtures using the Symfony CLI
-symfony console doctrine:fixtures:load --no-interaction
-
 # Start the built-in server using the Symfony CLI
-symfony server:start --port=8080
+symfony server:start --port=8000
 
