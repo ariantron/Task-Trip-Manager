681d86c8ec31f6614dd6335cc1c8de786e9a8249
Arian Tron <ariantron@yahoo.com>
2024-06-17 21:35:09 +0330

    chore: front, back

--- back-end/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Install PHP dependencies using Composer
-composer install
-
-# Start the PHP built-in web server
-php -S 0.0.0.0:8005 -t ./public
