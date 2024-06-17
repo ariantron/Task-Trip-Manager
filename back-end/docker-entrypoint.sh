17bcc6a77f08fac908b13263eae85bcd0fc68fa4
Arian Tron <ariantron@yahoo.com>
2024-06-17 22:24:17 +0330

    chore: back-docker

--- back-end/docker-entrypoint.sh
+++ back-end/docker-entrypoint.sh
@@ -6,13 +6,13 @@ set -e
 composer install
 
 # Create a new migration based on the current database schema
-php bin/console make:migration
+php bin/console make:migration --no-interaction
 
 # Apply the migrations to the database
-php bin/console doctrine:migrations:migrate
+php bin/console doctrine:migrations:migrate --no-interaction
 
 # Load data fixtures into the database
-php bin/console doctrine:fixtures:load
+php bin/console doctrine:fixtures:load --no-interaction
 
 # Start the PHP built-in web server
 php -S localhost:8000 -t ./public
