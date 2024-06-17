683b975ee298ce7b98c93c6649bc5a2af0c38dfd
Arian Tron <ariantron@yahoo.com>
2024-06-17 22:23:06 +0330

    chore: back-docker

---
+++ back-end/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Install PHP dependencies using Composer
+composer install
+
+# Create a new migration based on the current database schema
+php bin/console make:migration
+
+# Apply the migrations to the database
+php bin/console doctrine:migrations:migrate
+
+# Load data fixtures into the database
+php bin/console doctrine:fixtures:load
+
+# Start the PHP built-in web server
+php -S localhost:8000 -t ./public
